fc79152066b53ef5953e6a0f6f2841e57da3df51
mustakafaya <birfincanmustafa@gmail.com>
2023-01-13 01:15:47 +0300

    mac son

---
+++ functions/funcs1.go
@@ -0,0 +1,11 @@
+package functions
+
+func Fonks(sayi1 int, sayi2 int) (int, int, int, int) {
+
+	topla := sayi1 + sayi2
+	cikar := sayi1 - sayi2
+	carp := sayi1 * sayi2
+	bol := sayi1 / sayi2
+
+	return topla, cikar, carp, bol
+}
--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-	"moduleadi/slices"
+	"fmt"
+	"moduleadi/functions"
 )
+
+// "moduleadi/slices"
 
 func main() {
 
-	// package adi .fonskiyon adi ()
-	slices.Pizza()
+	say1 := 1
+	say2 := 1
+	fmt.Print("1. sayıyı giriniz;")
+	fmt.Scanln(&say1)
+	fmt.Print("2. sayıyı giriniz;")
+	fmt.Scanln(&say2)
+
+	sonuc1, sonuc2, sonuc3, sonuc4 := functions.Fonks(say1, say2)
+
+	fmt.Println("Toplamı : ", sonuc1)
+	fmt.Println("Farkı : ", sonuc2)
+	fmt.Println("Çarpımı : ", sonuc3)
+	fmt.Println("Bölümü : ", sonuc4)
 
 }
